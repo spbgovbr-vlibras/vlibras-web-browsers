c23ee39a10c3a228ff5583cf259db0cd6444cfcb
Mateus Pires <mateusplpl@gmail.com>
2017-05-08 16:19:18 -0300

    Update version, add missing images and create script to generate firefox plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-var gulp = require('gulp');
-var webpack = require('webpack-stream');
+const gulp = require('gulp');
+const webpack = require('webpack-stream');
+// const exec = require('gulp-exec');
+const exec = require('child_process').exec;
 
-var webpackConfig = require('./webpack.config.js');
+const webpackConfig = require('./webpack.config.js');
 
-var options = {
+const options = {
   dest: {
     chrome: 'chrome/app/player',
     firefox: 'firefox/data/player',
@@ -11,8 +13,11 @@ var options = {
   }
 };
 
+
+// BUILD
+
 function build(target, player_target) {
-  var destPath = options.dest[target];
+  const destPath = options.dest[target];
 
   gulp.src(player_target || 'node_modules/vlibras/src/target/**/*')
     .pipe(gulp.dest(destPath + '/target'));
@@ -38,3 +43,16 @@ gulp.task('build:safari', function () {
 });
 
 gulp.task('build', ['build:chrome', 'build:firefox', 'build:safari']);
+
+
+// GEN
+
+gulp.task('gen:firefox', function () {
+  exec('cd firefox & jpm xpi', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    console.log(err);
+  });
+});
+
+gulp.task('gen', ['gen:firefox']);
