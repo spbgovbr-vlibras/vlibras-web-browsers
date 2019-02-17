bd5c839b67efcbcd522f4b8b007c2d3e29f63238
Mateus Pires <mateusplpl@gmail.com>
2019-02-17 20:10:47 -0300

    Remove unused chrome and firefox specific source and fix extension window resize

--- firefox/data/delegator.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-self.port.on('selectedText', function(selectedText) {
-  var event = new CustomEvent('plugin:selectedText', { 'detail': selectedText });
-
-  document.dispatchEvent(event);
-});
--- firefox/data/middleware.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('plugin:selectedText', function(e) {
-  if (e.detail === undefined) return;
-
-  window.plugin = (window.plugin || new VLibras.Plugin());
-  window.plugin.translate(e.detail);
-});
