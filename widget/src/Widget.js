fb131ddeecd286f175ed687e28973a1ee68b112d
Mateus Pires <mateusplpl@gmail.com>
2019-03-29 08:03:45 -0300

    Fix reset styles

--- widget/src/Widget.js
+++ widget/src/Widget.js
@@ -2,43 +2,41 @@ const window = require('window');
 
 const AccessButton = require('./components/AccessButton');
 const PluginWrapper = require('./components/PluginWrapper');
-require('./styles.scss');
+
+require('./scss/reset.scss');
+require('./scss/styles.scss');
 
 module.exports = function Widget() {
   const widgetWrapper = new PluginWrapper();
   const accessButton = new AccessButton(widgetWrapper);
   
   window.onload = () => {
-    const vw = document.querySelector('[vw]');
-	const wrapper = document.querySelector('[vw-plugin-wrapper]');
-	const access = document.querySelector('[vw-access-button]');
+		const vw = document.querySelector('[vw]');
+		const wrapper = document.querySelector('[vw-plugin-wrapper]');
+		const access = document.querySelector('[vw-access-button]');
 
-    accessButton.load(document.querySelector('[vw-access-button]'), vw);
-    widgetWrapper.load(document.querySelector('[vw-plugin-wrapper]'));
+		accessButton.load(document.querySelector('[vw-access-button]'), vw);
+		widgetWrapper.load(document.querySelector('[vw-plugin-wrapper]'));
 
-	window.addEventListener('vp-widget-wrapper-set-side', (event) => { console.log(':', event.detail)
-		if(event.detail.right) {
-			vw.style.left = '0';
-			vw.style.right = 'initial';
-		} else {
-			vw.style.right = '0';
-			vw.style.left = 'initial';
-		}
+		window.addEventListener('vp-widget-wrapper-set-side', (event) => { console.log(':', event.detail)
+			if (event.detail.right) {
+				vw.style.left = '0';
+				vw.style.right = 'initial';
+			} else {
+				vw.style.right = '0';
+				vw.style.left = 'initial';
+			}
+		});
 
-	});
+		window.addEventListener('vp-widget-close', (event) => {
+			access.classList.toggle('active');
+			wrapper.classList.toggle('active');
 
-
-	window.addEventListener('vp-widget-close', (event) => { 
-
-		access.classList.toggle('active');
-    	wrapper.classList.toggle('active');
-		var tagsTexts = document.querySelectorAll('.vw-text');
-      	for (var i = 0; i < tagsTexts.length; i++) {
-	        var parent  = tagsTexts[i].parentNode;
-	        parent.innerHTML = tagsTexts[i].innerHTML;
-      	}	
-  	});
-
-    
+			var tagsTexts = document.querySelectorAll('.vw-text');
+			for (var i = 0; i < tagsTexts.length; i++) {
+				var parent  = tagsTexts[i].parentNode;
+				parent.innerHTML = tagsTexts[i].innerHTML;
+			}	
+		});
   };
 }
