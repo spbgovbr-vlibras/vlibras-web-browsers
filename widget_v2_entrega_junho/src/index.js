687b3d7dffd04dfdfd72eb38d53deca7aa2b515c
Suanny <suanny_fabyne@hotmail.com>
2019-06-26 18:02:45 -0300

    add stop on close

--- widget_v2_entrega_junho/src/index.js
+++
@@ -1,7 +0,0 @@
-const window = require('window');
-
-const Plugin = require('../../plugin/Plugin');
-const Widget = require('./Widget');
-
-window.VLibras.Plugin = Plugin;
-window.VLibras.Widget = Widget;
