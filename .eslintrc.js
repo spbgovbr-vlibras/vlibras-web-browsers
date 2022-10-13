af6361815ba67adecfeaf4854a2924460fa94f7b
suann <suann@DESKTOP-0D7GBOJ.local>
2022-10-12 21:32:09 -0300

    feat: configure updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-  },
-};
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "google",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    },
+    'ignorePatterns': ['**/widget/app', '**/plugin/targets'],
+}
