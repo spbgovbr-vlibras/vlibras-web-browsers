d2f44a7e9fc1aa9aa6cf7c6ad6b1af4e219eb46d
suanny fabyne <suanny@lavid.ufpb.br>
2022-10-12 22:32:03 -0300

    config: fix all eslint errors

--- safari.safariextension/app/middleware.js
+++ safari.safariextension/app/middleware.js
@@ -1,9 +1,12 @@
 window.resizeTo(540, 470);
 
-safari.self.addEventListener('message', function (request) {
-  if (request.name !== 'plugin:selectedText' && request.message === undefined) return;
-  
-  window.plugin = (window.plugin || new window.VLibras.Plugin({enableMoveWindow: false}));
+safari.self.addEventListener('message', function(request) {
+  if (request.name !== 'plugin:selectedText' && request.message === undefined) {
+    return;
+  }
+
+  window.plugin =
+    window.plugin || new window.VLibras.Plugin({enableMoveWindow: false});
   window.plugin.translate(request.message);
 });
 
