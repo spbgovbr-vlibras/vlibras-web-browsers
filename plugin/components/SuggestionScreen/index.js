8ee4a4124cf4f8f41fd0a620fa88d5fca57dcc6b
Suanny Fabyne <suanny_fabyne@hotmail.com>
2023-01-29 21:04:39 -0300

    feat: update version and set dict url

--- plugin/components/SuggestionScreen/index.js
+++ plugin/components/SuggestionScreen/index.js
@@ -193,7 +193,7 @@ SuggestionScreen.prototype.load = function (element) {
   });
 
   const xhr = new XMLHttpRequest();
-  xhr.open('get', 'https://repository-dth.vlibras.gov.br/api/signs', true);
+  xhr.open('get', 'https://repositorio.vlibras.gov.br/api/signs', true);
   xhr.responseType = 'text';
   xhr.onload = function () {
     if (xhr.status == 200) {
