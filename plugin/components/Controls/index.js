6f50fb97ec0889a28059018f4961763fef46db98
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-02-09 14:15:32 -0300

    fix: label of first translation adjusted

--- plugin/components/Controls/index.js
+++ plugin/components/Controls/index.js
@@ -4,6 +4,8 @@ require('nouislider/distribute/nouislider.min.css');
 const controlsTpl = require('./controls.html').default;
 require('./controls.scss');
 
+let firstTranslation = false;
+
 function Controls(player, dictionary) {
   this.player = player;
   this.dictionary = dictionary;
@@ -71,10 +73,11 @@ function Controls(player, dictionary) {
       });
 
       slider.noUiSlider.set([globalGlosaLenght, globalGlosaLenght]);
+      this.element.classList.remove('vpw-selectText');
     }.bind(this)
   );
 
-  let firstTranslation = false;
+
   this.player.on(
     'stop:welcome',
     function () {
@@ -221,3 +224,4 @@ Controls.prototype.setProgress = function () {
 };
 
 module.exports = Controls;
+
