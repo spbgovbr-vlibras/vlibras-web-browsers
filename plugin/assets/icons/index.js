0eeeb908a20bf7e626db42315542a18bcd2469b3
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-03-15 20:38:25 -0300

    feat: updated icons

---
+++ plugin/assets/icons/index.js
@@ -0,0 +1,5 @@
+const playIcon = require('./play.svg');
+const pauseIcon = require('./pause.svg');
+const restartIcon = require('./restart.svg');
+
+module.exports = { playIcon, pauseIcon, restartIcon };
