2496face94544d4dc5dca2840f7fecfe71fe9065
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-03-22 13:57:13 -0300

    feat: controls component update

--- plugin/assets/icons/index.js
+++ plugin/assets/icons/index.js
@@ -1,5 +1,6 @@
 const playIcon = require('./play.svg');
 const pauseIcon = require('./pause.svg');
 const restartIcon = require('./restart.svg');
+const subtitleIcon = require('./subtitle.svg');
 
-module.exports = { playIcon, pauseIcon, restartIcon };
+module.exports = { playIcon, pauseIcon, restartIcon, subtitleIcon };
