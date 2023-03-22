979e3a43135394ac5f8c7ae7b6da2a19c4a94338
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-03-22 16:15:52 -0300

    feat: changeAvatar component update

--- plugin/assets/icons/index.js
+++ plugin/assets/icons/index.js
@@ -2,5 +2,16 @@ const playIcon = require('./play.svg');
 const pauseIcon = require('./pause.svg');
 const restartIcon = require('./restart.svg');
 const subtitleIcon = require('./subtitle.svg');
+const IcaroIcon = require('./avatars/icaro.svg');
+const hozanaIcon = require('./avatars/hozana.svg');
+const gugaIcon = require('./avatars/guga.svg');
 
-module.exports = { playIcon, pauseIcon, restartIcon, subtitleIcon };
+module.exports = { 
+  playIcon, 
+  pauseIcon, 
+  restartIcon, 
+  subtitleIcon,
+  IcaroIcon,
+  hozanaIcon,
+  gugaIcon
+};
