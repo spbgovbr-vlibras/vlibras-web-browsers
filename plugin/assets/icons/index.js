8e481e396570749de60340f78d4d38076d87a801
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-03-23 16:32:47 -0300

    feat: add vw header icons

--- plugin/assets/icons/index.js
+++ plugin/assets/icons/index.js
@@ -5,6 +5,10 @@ const subtitleIcon = require('./subtitle.svg');
 const IcaroIcon = require('./avatars/icaro.svg');
 const hozanaIcon = require('./avatars/hozana.svg');
 const gugaIcon = require('./avatars/guga.svg');
+const aboutIcon = require('./header/about.svg');
+const closeIcon = require('./header/close.svg');
+const dictionaryIcon = require('./header/dictionary.svg');
+const settingsIcon = require('./header/settings.svg');
 
 module.exports = { 
   playIcon, 
@@ -13,5 +17,9 @@ module.exports = {
   subtitleIcon,
   IcaroIcon,
   hozanaIcon,
-  gugaIcon
+  gugaIcon,
+  aboutIcon,
+  closeIcon,
+  dictionaryIcon,
+  settingsIcon
 };
