c90a5d2f48f9f36bc87011ad57fd0157cc514953
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-05-31 02:09:31 -0300

    fix: homolog review url

--- plugin/config.js
+++ plugin/config.js
@@ -5,5 +5,5 @@
 
 // homolog urls
 exports.DICTIONARY_URL = 'https://dicionario2-dth.vlibras.gov.br/signs?version=2018.3.1';
-exports.REVIEW_URL = 'https://traducao2-dth.vlibras.gov.br/review';
+exports.REVIEW_URL = 'https://traducao2-dth.vlibras.gov.br/dl/review';
 exports.SIGNS_URL = 'https://repositorio-dth.vlibras.gov.br/api/signs';
