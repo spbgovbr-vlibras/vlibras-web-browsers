66e763a0296586319cb492f61fb46526cca2c292
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-07-13 13:34:12 -0300

    feat: updated new features

---
+++ plugin/components/AdditionalOptions/WidgetHelp/index.js
@@ -0,0 +1,23 @@
+const template = require('./widget-help.html').default;
+require('./widget-help.scss');
+
+function WidgetHelp(player) {
+  this.element = null;
+  this.player = player;
+}
+
+WidgetHelp.prototype.load = function (element) {
+  this.element = element;
+  this.element.innerHTML = template;
+
+}
+
+WidgetHelp.prototype.show = function () {
+  this.element.classList.add('vp-enabled');
+}
+
+WidgetHelp.prototype.hide = function () {
+  this.element.classList.remove('vp-enabled');
+}
+
+module.exports = WidgetHelp;
