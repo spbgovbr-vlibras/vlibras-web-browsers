35db5b9e9cbe0c191b44ec1fc0d4da954b1ac5b2
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-08-10 10:17:13 -0300

    feat: add utils items

---
+++ plugin/utils/index.js
@@ -0,0 +1,21 @@
+const isFullscreen = () => {
+  return hasClass(document.body, 'vpw-fullscreen');
+}
+
+const isPlaying = () => {
+  return hasClass($('div[vp-controls]'), 'vpw-playing');
+}
+
+const widgetPosition = () => {
+  return window.plugin ? window.plugin.position : undefined;
+}
+
+const $ = (path, element = null) => {
+  return element ? element.querySelector(path) : $(path, document);
+}
+
+const hasClass = (element, clss) => {
+  element ? element.classList.contains(clss) : undefined;
+}
+
+export { isPlaying, isFullscreen, $, hasClass, widgetPosition }
