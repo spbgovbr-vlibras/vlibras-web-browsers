11c7fade99b76aedb085bd1be8e0eb3bdd4f2c5b
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-08-14 03:26:18 -0300

    wip: update widget help

--- plugin/components/AdditionalOptions/WidgetHelp/tutorialElements.js
+++ plugin/components/AdditionalOptions/WidgetHelp/tutorialElements.js
@@ -1,24 +1,24 @@
 export const tutorialElements = [
   {
-    path: '.vpw-box',
+    path: 'div[vp-box]',
     text: `Clique nos botões do cabeçalho para acessar as Configurações, 
     pesquisar sinais no Dicionário ou saber mais sobre o VLibras.`
   },
   {
     path: 'div[vp-change-avatar]',
-    container: '',
-    text: `Clique nesse botão para alterar o avatar. 
+    center: true,
+    text: `Clique neste botão para alterar o avatar. 
     Você poderá poderá escolher entre Ícaro, Guga e Hosana.`
   },
   {
     path: '.vpw-translator-button',
-    container: '',
+    center: true,
     text: `Precisa traduzir um texto específico? 
-    Clique aqui para abrir o serviço de Tradutor do VLibras Widget.`
+    Clique neste botão para abrir o Tradutor do VLibras Widget.`
   },
   {
     path: 'div[vp-controls]',
-    text: `Clique nos botões do rodapé para pausar, retomar animação, 
-    ajustar velocidade, ativar legendas e abrir o widget em tela cheia.`
+    text: `Clique nos botões de controle para parar ou retomar animação, 
+    ajustar velocidade, ativar legendas e deixar o avatar em tela cheia.`
   },
 ]
