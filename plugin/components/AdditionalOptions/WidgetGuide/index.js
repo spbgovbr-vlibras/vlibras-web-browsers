5d8cdd3d2ea845a1f6d8eda61f52c5f2554c42ca
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-08-15 12:58:02 -0300

    code refactor

--- plugin/components/AdditionalOptions/WidgetGuide/index.js
+++ plugin/components/AdditionalOptions/WidgetGuide/index.js
@@ -2,9 +2,9 @@
 const template = require('./widget-guide.html').default;
 require('./widget-guide.scss');
 
+const utils = require('~utils');
 const { closeIcon } = require('~icons');
-const { tutorialElements } = require('./tutorialElements');
-const { isFullscreen, $, addClass, removeClass, getRect } = require('~utils');
+const { guideElements } = require('./guide-elements');
 
 let vw = null;
 
@@ -26,12 +26,12 @@ function WidgetGuide(player) {
 WidgetGuide.prototype.load = function (element) {
   this.element = element;
   this.element.innerHTML = template;
-  this.helpButton = $('.vpw-help-button', $('div[vw]'));
-  this.message = $('.vpw-tutorial__message', this.element);
-  this.backButton = $('.vpw-tutorial__back-btn', this.element);
-  this.nextButton = $('.vpw-tutorial__next-btn', this.element);
-  this.closeButton = $('.vpw-tutorial__close-btn', this.element);
-  this.tabSlider = $('.vpw-tutorial__tab-slider', this.element);
+  this.helpButton = utils.$('.vpw-help-button', utils.$('div[vw]'));
+  this.message = utils.$('.vpw-tutorial__message', this.element);
+  this.backButton = utils.$('.vpw-tutorial__back-btn', this.element);
+  this.nextButton = utils.$('.vpw-tutorial__next-btn', this.element);
+  this.closeButton = utils.$('.vpw-tutorial__close-btn', this.element);
+  this.tabSlider = utils.$('.vpw-tutorial__tab-slider', this.element);
 
   this.closeButton.innerHTML = closeIcon;
 
@@ -39,8 +39,8 @@ WidgetGuide.prototype.load = function (element) {
   this.backButton.onclick = () => this.back();
   this.nextButton.onclick = () => this.next();
 
-  vw = $('div[vw]');
-  tutorialElements.forEach(({ path }) => this.$elements.push($(path)));
+  vw = utils.$('div[vw]');
+  guideElements.forEach(({ path }) => this.$elements.push(utils.$(path)));
 }
 
 WidgetGuide.prototype.show = function () {
@@ -51,8 +51,8 @@ WidgetGuide.prototype.show = function () {
   callWidgetTranslator.bind(this)();
   fixedButtons();
   addClickBlocker(true);
-  addClass(this.helpButton, 'vp-selected');
-  removeClass($('div[vp-change-avatar]'), 'vp-change-avatar-openned');
+  utils.addClass(this.helpButton, 'vp-selected');
+  utils.removeClass(utils.$('div[vp-change-avatar]'), 'vp-change-avatar-openned');
 }
 
 WidgetGuide.prototype.hide = function () {
@@ -63,8 +63,8 @@ WidgetGuide.prototype.hide = function () {
   this.player.gloss = undefined;
   resetItems();
   addClickBlocker(false);
-  removeClass(this.helpButton, 'vp-selected');
-  changeWidgetPosition(this.wPosition);
+  utils.removeClass(this.helpButton, 'vp-selected');
+  utils.setWidgetPosition(this.wPosition);
 }
 
 WidgetGuide.prototype.toggle = function () {
@@ -73,7 +73,7 @@ WidgetGuide.prototype.toggle = function () {
 }
 
 WidgetGuide.prototype.next = function () {
-  if (this.tab === tutorialElements.length - 1) return this.hide();
+  if (this.tab === guideElements.length - 1) return this.hide();
   this.tab++;
   this.updatePos();
   updateButtons.bind(this)();
@@ -89,7 +89,7 @@ WidgetGuide.prototype.back = function () {
 }
 
 WidgetGuide.prototype.restart = function () {
-  this.message.innerHTML = tutorialElements[0].text;
+  this.message.innerHTML = guideElements[0].text;
   this.backButton.setAttribute('disabled', true);
   this.nextButton.innerHTML = 'Avançar';
   this.tab = 0;
@@ -99,9 +99,9 @@ WidgetGuide.prototype.updatePos = function () {
   const position = window.plugin.position;
   const isLeft = position.includes('L');
   const item = this.$elements[this.tab];
-  const { top: wTop, width: wWidth, height: wHeight } = getRect(vw);
-  const { top: iTop, height: iHeight } = getRect(item);
-  const { width: eWidth, height: eHeight } = getRect(this.element);
+  const { top: wTop, width: wWidth, height: wHeight } = utils.getRect(vw);
+  const { top: iTop, height: iHeight } = utils.getRect(item);
+  const { width: eWidth, height: eHeight } = utils.getRect(this.element);
 
   // Set tab id
   this.tabSlider.innerHTML = `${this.tab + 1}/${this.$elements.length}`;
@@ -113,15 +113,15 @@ WidgetGuide.prototype.updatePos = function () {
   const height = wHeight;
   const top = !item ? wTop : isLowerView ? (iTop - eHeight + iHeight) : iTop;
 
-  if (!isFullscreen()) {
+  if (!utils.isFullscreen()) {
     if (fitInHalfWindow() && 'TB'.includes(this.wPosition)) {
-      addClass(this.element, 'vw-centered');
+      utils.addClass(this.element, 'vw-centered');
       this.element.style.top = top + 'px';
-      changeWidgetPosition(this.wPosition);
+      utils.setWidgetPosition(this.wPosition);
       updateArrow.bind(this)();
 
     } else if (window.innerWidth >= 600) {
-      removeClass(this.element, 'vw-centered');
+      utils.removeClass(this.element, 'vw-centered');
       this.element.style.left = isLeft ? width + 30 + 'px' : 'initial';
       this.element.style.right = !isLeft ? width + 30 + 'px' : 'initial';
       this.element.style.top = top + 'px';
@@ -130,36 +130,36 @@ WidgetGuide.prototype.updatePos = function () {
       updateArrow.bind(this)();
 
       const wp = this.wPosition;
-      changeWidgetPosition('TB'.includes(wp) ? wp + 'R' : wp)
+      utils.setWidgetPosition('TB'.includes(wp) ? wp + 'R' : wp)
 
     } else {
-      changeWidgetPosition('T');
-      removeClass(this.element, 'vw-centered');
-      maxWidth.bind(this)(40);
+      utils.setWidgetPosition('T');
+      utils.removeClass(this.element, 'vw-centered');
+      expandGuide.bind(this)(40);
       this.element.style.top = height + 20 + 'px';
       this.element.style.bottom = 'initial';
     }
   } else {
     // is fullscreen
-    maxWidth.bind(this)(10);
-    removeClass(this.element, 'vw-centered');
+    expandGuide.bind(this)(10);
+    utils.removeClass(this.element, 'vw-centered');
     this.element.style.top = 'initial';
     this.element.style.bottom = '58px';
   }
 
-  function maxWidth(margin) {
+  function expandGuide(margin) {
     this.element.style.left = margin + 'px';
     this.element.style.right = margin + 'px';
     this.element.style.maxWidth = '100vw';
-    addClass(this.element, 'not-arrow');
+    utils.addClass(this.element, 'not-arrow');
   }
 
   function updateArrow() {
-    addClass(this.element, `vw-${isLeft ? 'left' : 'right'}`);
-    removeClass(this.element, `vw-${!isLeft ? 'left' : 'right'}`);
-    addClass(this.element, `vw-${isLowerView ? 'bottom' : 'top'}`);
-    removeClass(this.element, `vw-${!isLowerView ? 'bottom' : 'top'}`);
-    removeClass(this.element, 'not-arrow');
+    utils.addClass(this.element, `vw-${isLeft ? 'left' : 'right'}`);
+    utils.removeClass(this.element, `vw-${!isLeft ? 'left' : 'right'}`);
+    utils.addClass(this.element, `vw-${isLowerView ? 'bottom' : 'top'}`);
+    utils.removeClass(this.element, `vw-${!isLowerView ? 'bottom' : 'top'}`);
+    utils.removeClass(this.element, 'not-arrow');
   }
 
   function fitInHalfWindow() {
@@ -171,38 +171,33 @@ WidgetGuide.prototype.updatePos = function () {
 function updateButtons() {
   if (this.tab === 0) this.backButton.setAttribute('disabled', true);
   else this.backButton.removeAttribute('disabled');
-  this.message.innerHTML = tutorialElements[this.tab].text;
-  this.nextButton.innerHTML = this.tab === tutorialElements.length - 1 ?
+  this.message.innerHTML = guideElements[this.tab].text;
+  this.nextButton.innerHTML = this.tab === guideElements.length - 1 ?
     'Concluir' : 'Avançar';
 }
 
 function callWidgetTranslator() {
-  this.player.translate(tutorialElements[this.tab].text);
-}
-
-function changeWidgetPosition(position) {
-  window.dispatchEvent(new CustomEvent('vp-widget-wrapper-set-side',
-    { detail: position }));
+  this.player.translate(guideElements[this.tab].text);
 }
 
 function addClickBlocker(bool) {
-  const element = $('span[vp-click-blocker]');
-  if (bool) addClass(element, 'vp-enabled');
-  else removeClass(element, 'vp-enabled');
+  const element = utils.$('span[vp-click-blocker]');
+  if (bool) utils.addClass(element, 'vp-enabled');
+  else utils.removeClass(element, 'vp-enabled');
 }
 
 function resetItems() {
-  $('div[vp-rate-box]').style.display = 'block';
-  removeClass($('div[vp-rate-box]'), 'vp-enabled');
-  removeClass($('div[vp-change-avatar]'), 'vp-fixed');
-  removeClass($('div[vp-additional-options]'), 'vp-fixed');
+  utils.$('div[vp-rate-box]').style.display = 'block';
+  utils.removeClass(utils.$('div[vp-rate-box]'), 'vp-enabled');
+  utils.removeClass(utils.$('div[vp-change-avatar]'), 'vp-fixed');
+  utils.removeClass(utils.$('div[vp-additional-options]'), 'vp-fixed');
 
 }
 
 function fixedButtons() {
-  $('div[vp-rate-box]').style.display = 'none';
-  addClass($('div[vp-change-avatar]'), 'vp-fixed');
-  addClass($('div[vp-additional-options]'), 'vp-fixed');
+  utils.$('div[vp-rate-box]').style.display = 'none';
+  utils.addClass(utils.$('div[vp-change-avatar]'), 'vp-fixed');
+  utils.addClass(utils.$('div[vp-additional-options]'), 'vp-fixed');
 }
 
 module.exports = WidgetGuide;
