1b3ec2a0d4fabd1f7ab1cefa7c1c68a8dfe7010b
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-08-18 07:29:57 -0300

    code refactor

--- plugin/components/AdditionalOptions/WidgetGuide/guide-elements.js
+++ plugin/components/AdditionalOptions/WidgetGuide/guide-elements.js
@@ -6,19 +6,17 @@ export const guideElements = [
   },
   {
     path: 'div[vp-change-avatar]',
-    center: true,
     text: `Clique neste botão para alterar o avatar. 
     Você poderá poderá escolher entre Ícaro, Guga e Hosana.`
   },
   {
     path: '.vpw-translator-button',
-    center: true,
     text: `Precisa traduzir um texto específico? 
     Clique neste botão para abrir o Tradutor do VLibras Widget.`
   },
   {
     path: 'div[vp-controls]',
     text: `Clique nos botões de controle para parar ou retomar animação, 
-    ajustar velocidade, ativar legendas e deixar o avatar em tela cheia.`
+    ajustar velocidade, ativar legendas e abrir o avatar em tela cheia.`
   },
 ]
