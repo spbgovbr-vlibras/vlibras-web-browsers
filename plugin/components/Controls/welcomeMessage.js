725ccb77a80cb5c46b23e52d653fea7ec67c4ae4
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-10-06 18:01:32 -0300

    fix: welcome texts timing

--- plugin/components/Controls/welcomeMessage.js
+++ plugin/components/Controls/welcomeMessage.js
@@ -3,21 +3,21 @@ export const welcomeMessage = {
   icaro: [
     { m: 'Olá, tudo bem?', t: 0 },
     { m: 'Meu nome é Ícaro.', t: 3.5 },
-    { m: 'Este é meu sinal.', t: 9 },
-    { m: 'Estou aqui para te ajudar<br>traduzindo textos para Libras.', t: 13.5 },
+    { m: 'Este é meu sinal.', t: 12 },
+    { m: 'Estou aqui para te ajudar<br>traduzindo textos para Libras.', t: 16 },
   ],
 
   hosana: [
     { m: 'Olá, tudo bem?', t: 0 },
-    { m: 'Meu nome é Hosana.', t: 4.5 },
-    { m: 'Este é meu sinal.', t: 10 },
-    { m: 'Estou aqui para te ajudar<br>traduzindo textos para Libras.', t: 14 },
+    { m: 'Meu nome é Hosana.', t: 3.5 },
+    { m: 'Este é meu sinal.', t: 13 },
+    { m: 'Estou aqui para te ajudar<br>traduzindo textos para Libras.', t: 18 },
   ],
 
   guga: [
     { m: 'Olá, tudo bem?', t: 0 },
-    { m: 'Meu nome é Guga.', t: 4.5 },
-    { m: 'Este é meu sinal.', t: 10 },
-    { m: 'Estou aqui para te ajudar<br>traduzindo textos para Libras.', t: 16 },
+    { m: 'Meu nome é Guga.', t: 3.5 },
+    { m: 'Este é meu sinal.', t: 13 },
+    { m: 'Estou aqui para te ajudar<br>traduzindo textos para Libras.', t: 19 },
   ]
 }
