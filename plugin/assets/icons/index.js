11f7c2989f772fa32acebbe2f243523b70dec696
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-10-06 12:21:05 -0300

    feat: add arrow-outward icon

--- plugin/assets/icons/index.js
+++ plugin/assets/icons/index.js
@@ -12,6 +12,7 @@ const likeSolidIcon = require('./feedback/like-solid.svg');
 const loadingIcon = require('./loading.svg');
 const translatorIcon = require('./additionalOptions/translator.svg');
 const helpIcon = require('./additionalOptions/help.svg');
+const arrowOutward = require('./feedback/arrow-outward.svg');
 
 const controlIcons = {
   play: require('./controls/play.svg'),
@@ -42,6 +43,7 @@ module.exports = {
   IcaroIcon, HosanaIcon, GugaIcon, aboutIcon, closeIcon,
   dictionaryIcon, settingsIcon, backIcon, socialIcons,
   positionIcons, arrowIcon, likeLineIcon, likeSolidIcon,
-  loadingIcon, translatorIcon, helpIcon, controlIcons
+  loadingIcon, translatorIcon, helpIcon, controlIcons,
+  arrowOutward
 }
 
