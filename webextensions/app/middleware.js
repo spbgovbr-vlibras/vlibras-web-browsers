de9f49a22def1259c992c14cb3b2ccf210ad6048
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-10-10 16:53:46 -0300

    add sandbox

--- webextensions/app/middleware.js
+++ webextensions/app/middleware.js
@@ -1,11 +1,14 @@
-window.addEventListener('load', function() {
-  chrome.runtime.onMessage.addListener(
-      function(request, sender, sendResponse) {
-        if (request.selectedText === undefined) return;
+window.addEventListener('load', function () {
+  // chrome.runtime.onMessage.addListener(
+  //   function (request, sender, sendResponse) {
+  //     if (request.selectedText === undefined) return;
 
-        window.plugin = (window.plugin || new window.VLibras.Plugin({enableMoveWindow: false}));
-        window.plugin.translate(request.selectedText);
-      });
+  //     window.plugin = (window.plugin || new window.VLibras.Plugin({ enableMoveWindow: false }));
+  //     window.plugin.translate(request.selectedText);
+  //   });
 
-  chrome.runtime.sendMessage({ready: true});
+  // chrome.runtime.sendMessage({ ready: true });
+
+  window.plugin = (window.plugin || new window.VLibras.Plugin({ enableMoveWindow: false }));
+
 });
