94eb30d6f4963c45970448af905f736eaad2b23f
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-10-26 12:28:30 -0300

    feat: text passing with postMessage

--- webextensions/app/middleware.js
+++ webextensions/app/middleware.js
@@ -1,11 +1,10 @@
 window.addEventListener('load', function () {
   const iframe = document.querySelector('iframe');
-  const src = 'app/player/index.html';
 
   chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
       if (request.selectedText === undefined) return;
-      iframe.src = src + '?text=' + request.selectedText;
+      iframe.contentWindow.postMessage(request.selectedText, '*');
     });
   chrome.runtime.sendMessage({ ready: true });
 });
