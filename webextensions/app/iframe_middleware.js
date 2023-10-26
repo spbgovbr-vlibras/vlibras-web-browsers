f15cb34209c5cd7beb156f24a3e94ae3d9502d23
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-10-26 12:28:30 -0300

    feat: text passing with postMessage

--- webextensions/app/iframe_middleware.js
+++ webextensions/app/iframe_middleware.js
@@ -1,25 +1,21 @@
-window.addEventListener('load', function () {
-  // chrome.runtime.onMessage.addListener(
-  //   function (request, sender, sendResponse) {
-  //     if (request.selectedText === undefined) return;
+let isLoaded = false;
 
-  //     window.plugin = (window.plugin || new window.VLibras.Plugin({ enableMoveWindow: false }));
-  //     window.plugin.translate(request.selectedText);
-  //   });
-
-  // chrome.runtime.sendMessage({ ready: true });
-
+window.addEventListener('load', () => {
   window.plugin = (window.plugin || new window.VLibras.Plugin({ enableMoveWindow: false }));
-
-
-  window.plugin.player.on('load', () => {
-    window.plugin.player.stop();
-
-    setTimeout(() => {
-      const text = new URL(location.href).searchParams.get('text');
-      window.plugin.translate(text);
-    }, 4000);
-
-  })
-
 });
+
+window.addEventListener('message', ({ data }) => {
+  if (!isLoaded) boundTranslate(data);
+  else translate(data);
+});
+
+function boundTranslate(text) {
+  window.plugin.player.on('load', () => {
+    setTimeout(() => translate(text), 4000);
+    isLoaded = true;
+  })
+}
+
+function translate(text) {
+  window.plugin.player.translate(text);
+}
