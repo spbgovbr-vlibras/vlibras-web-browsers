0af42e83ed6df1b615d99d623f4f141f073c8fc7
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-11-10 14:17:59 -0300

    chore: add "~constants" path

---
+++ plugin/constants/homolog-paths.js
@@ -0,0 +1,8 @@
+module.exports = {
+  PUBLIC_PATH: 'https://www-dth.vlibras.gov.br/app/',
+  ROOT_PATH: 'https://www-dth.vlibras.gov.br/app/',
+
+  DICTIONARY_URL: 'https://dicionario2-dth.vlibras.gov.br/signs?version=2018.3.1',
+  REVIEW_URL: 'https://traducao2-dth.vlibras.gov.br/dl/review',
+  SIGNS_URL: 'https://repositorio-dth.vlibras.gov.br/api/signs',
+}
---
+++ plugin/constants/production-paths.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // PUBLIC_PATH: 'https://www.vlibras.gov.br/app/',
+  // ROOT_PATH: 'https://www.vlibras.gov.br/app/',
+
+  PUBLIC_PATH: 'https://vlibras.gov.br/doc/app/',
+  ROOT_PATH: 'https://vlibras.gov.br/doc/app/',
+
+  DICTIONARY_URL: 'https://dicionario2.vlibras.gov.br/signs?version=2018.3.1',
+  REVIEW_URL: 'https://traducao2.vlibras.gov.br/review',
+  SIGNS_URL: 'https://dicionario2.vlibras.gov.br/bundles',
+}
