e6b2d5fee649c089130cbc0da1de4af1fefe5f0c
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-12-02 02:02:27 -0300

    fix: translation logic

--- webextensions/firefox.extension/app/middleware.js
+++ webextensions/firefox.extension/app/middleware.js
@@ -1,12 +1,16 @@
+let isPlayerLoaded = false;
+
 window.addEventListener('load', function () {
   chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
       if (request.selectedText === undefined) return;
 
       window.plugin = (window.plugin || new window.VLibras.Plugin({ enableMoveWindow: false }));
+      if (isPlayerLoaded) return window.plugin.translate(request.selectedText);
 
       window.plugin.player.on('load', () => {
-        setTimeout(() => window.plugin.translate(request.selectedText), 3000);
+        setTimeout(() => window.plugin.translate(request.selectedText), 2000);
+        isPlayerLoaded = true;
       })
 
     });
