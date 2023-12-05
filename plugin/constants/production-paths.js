d046d0b3483227eed2ff027f8ab26ec1acaec4a5
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-12-05 16:51:16 -0300

    refactor: production root path

--- plugin/constants/production-paths.js
+++ plugin/constants/production-paths.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // ROOT_PATH: 'https://www.vlibras.gov.br/app/',
-  ROOT_PATH: 'https://vlibras.gov.br/doc/app/',
+  ROOT_PATH: 'https://www.vlibras.gov.br/app/',
 
   DICTIONARY_URL: 'https://dicionario2.vlibras.gov.br/signs?version=2018.3.1',
   REVIEW_URL: 'https://traducao2.vlibras.gov.br/review',
