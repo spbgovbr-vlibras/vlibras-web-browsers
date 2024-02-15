6c5e0c2b20127fdd5867044cbc11e357276f0df5
Diêgo Ferreira <diego.raian@lavid.ufpb.br>
2024-02-15 19:43:09 -0300

    refactor: removed re-ping blocker

--- widget/src/services/index.js
+++ widget/src/services/index.js
@@ -1,24 +1,9 @@
 const { PING_URL } = require('~widget-constants');
 
-const WIDGET_PING_KEY = '@widget-pinged';
-
 export function sendWidgetPing() {
-  if (hasPinged()) return;
-
   fetch(PING_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: `{ "site": "${location.hostname}" }`,
-  })
-    .then((res) => res.json())
-    .then(() => setPinged())
-    .catch((err) => console.error(err));
-
-  function hasPinged() {
-    return localStorage.getItem(WIDGET_PING_KEY) === 'true';
-  }
-
-  function setPinged() {
-    localStorage.setItem(WIDGET_PING_KEY, 'true');
-  }
+  }).catch((err) => console.error(err));
 }
