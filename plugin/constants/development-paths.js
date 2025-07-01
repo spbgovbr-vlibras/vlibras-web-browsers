87157833044c88f617135ec712893954baf52e8b
Diêgo Ferreira <diego.raian@lavid.ufpb.br>
2025-07-01 15:53:27 -0300

    fix: update api endpoints for dictionary and signs

--- plugin/constants/development-paths.js
+++ plugin/constants/development-paths.js
@@ -3,5 +3,5 @@ module.exports = {
   
   DICTIONARY_URL: 'https://dicionario2-dth.lavid.ufpb.br/signs?version=2018.3.1',
   REVIEW_URL: 'https://traducao2-dth.vlibras.lavid.ufpb.br/dl/review',
-  SIGNS_URL: 'https://repositorio-dth.vlibras.lavid.ufpb.br/api/signs',
+  SIGNS_URL: 'https://dicionario2-dth.vlibras.gov.br/bundles',
 };
