9554ad6f3d983d93832081a6eed436228eeb9d37
Diêgo Ferreira <diegofrr.19@gmail.com>
2026-03-22 14:19:47 -0300

    chore(config): update config urls

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -4,24 +4,30 @@ const REQUEST_TIMEOUT = 10000;
 
 const envConfigs = {
 	development: {
-		TRANSLATE_URL: "https://traducao2-dev.vlibras.gov.br",
-		DICTIONARY_URL: "https://dicionario2-dev.vlibras.gov.br",
-		REVIEW_URL: "https://review2-dev.vlibras.gov.br",
+		TRANSLATE_URL: "https://traducao2-dth.vlibras.gov.br/translate",
+		DICTIONARY_URL: "https://dicionario2-dth.vlibras.gov.br/2018.3.1/WEBGL/",
+		DICTIONARY_STATIC_URL: "https://dicionario2-dth.vlibras.gov.br/static/BUNDLES/2018.3.1/WEBGL",
 		SIGNS_URL: "https://dicionario2.vlibras.gov.br/static/TREES/2018.3.1.json",
+		REVIEW_URL: "https://review2-dev.vlibras.gov.br",
+		BUNDLES_URL: "https://dicionario2-dth.vlibras.gov.br/bundles",
 	},
 
 	dth: {
-		TRANSLATE_URL: "https://traducao2-dth.vlibras.gov.br",
-		DICTIONARY_URL: "https://dicionario2.vlibras.gov.br",
-		REVIEW_URL: "https://review2.vlibras.gov.br",
+		TRANSLATE_URL: "https://traducao2-dth.vlibras.gov.br/translate",
+		DICTIONARY_URL: "https://dicionario2-dth.vlibras.gov.br/2018.3.1/WEBGL",
+		DICTIONARY_STATIC_URL: "https://dicionario2-dth.vlibras.gov.br/static/BUNDLES/2018.3.1/WEBGL",
 		SIGNS_URL: "https://dicionario2.vlibras.gov.br/static/TREES/2018.3.1.json",
+		REVIEW_URL: "https://traducao2-dth.vlibras.gov.br/dl/review",
+		BUNDLES_URL: "https://dicionario2-dth.vlibras.gov.br/bundles",
 	},
 
 	production: {
-		TRANSLATE_URL: "https://traducao2.vlibras.gov.br",
-		DICTIONARY_URL: "https://dicionario2.vlibras.gov.br",
-		REVIEW_URL: "https://review2.vlibras.gov.br",
+		TRANSLATE_URL: "https://traducao2.vlibras.gov.br/translate",
+		DICTIONARY_URL: "https://dicionario2.vlibras.gov.br/2018.3.1/WEBGL",
+		DICTIONARY_STATIC_URL: "https://dicionario2.vlibras.gov.br/static/BUNDLES/2018.3.1/WEBGL",
 		SIGNS_URL: "https://dicionario2.vlibras.gov.br/static/TREES/2018.3.1.json",
+		REVIEW_URL: "https://traducao2.vlibras.gov.br/review",
+		BUNDLES_URL: "https://dicionario2.vlibras.gov.br/bundles",
 	},
 };
 
