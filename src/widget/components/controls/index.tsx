e77cee5a346107cba41ab429f20e3839f12f2a7c
Diêgo Ferreira <diegofrr.19@gmail.com>
2026-03-22 13:44:04 -0300

    feat(widget): add draggable widget grabber

--- src/widget/components/controls/index.tsx
+++ src/widget/components/controls/index.tsx
@@ -4,24 +4,28 @@ import { Button } from "@/widget/components/ui/button";
 import { Tooltip } from "@/widget/components/ui/tooltip";
 import { PauseIcon, PlayIcon, SettingsIcon } from "@/widget/icons";
 import { useScreensStore } from "@/widget/stores/use-screens.store";
+import { useWidgetStore } from "@/widget/stores/use-widget.store";
 
 export const WidgetControls = () => {
-	const { openScreen } = useScreensStore();
+	const { open } = useScreensStore();
 	const { play, pause, status, repeat, gloss } = usePlayer();
+	const { isOpen } = useWidgetStore();
 
 	return (
 		<div
 			className={cn(
 				"absolute inset-2 top-auto flex animate-move-up items-center justify-between gap-1 transition-[bottom] ease-in-out [&>div]:h-11.5 [&>div]:p-1",
 				"[&_button>svg]:size-5 [&_button]:rounded-full [&_button]:text-primary",
-				!open && "-bottom-20!",
+				!isOpen && "-bottom-20!",
 			)}
 		>
 			<div
 				style={{ boxShadow: "0 0 10px rgba(0, 0, 0, 0.15)" }}
 				className={cn("flex w-full items-center gap-1 rounded-full border bg-background")}
 			>
-				{status === "idle" && !gloss && <div className="w-full text-center text-sm">Aguardando texto...</div>}
+				{status === "idle" && !gloss && (
+					<div className="w-full text-center text-foreground text-sm">Aguardando texto...</div>
+				)}
 
 				{status === "paused" && (
 					<Tooltip
@@ -73,7 +77,7 @@ export const WidgetControls = () => {
 				style={{ boxShadow: "0 0 10px rgba(0, 0, 0, 0.15)" }}
 				className="flex items-center gap-1 rounded-full border bg-background"
 			>
-				<Button onClick={() => openScreen("settings")} variant="ghost" size="icon">
+				<Button onClick={() => open("settings")} variant="ghost" size="icon">
 					<SettingsIcon className="text-foreground" />
 				</Button>
 			</div>
