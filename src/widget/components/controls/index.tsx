9f16f848dc33d01fcf3ea6baf49f32cea251f5ed
Diêgo Ferreira <diegofrr.19@gmail.com>
2026-03-23 22:33:49 -0300

    feat(widget): add waiting state and update control layout

--- src/widget/components/controls/index.tsx
+++ src/widget/components/controls/index.tsx
@@ -1,16 +1,17 @@
+import { useMemo } from "preact/hooks";
 import { Fragment } from "preact/jsx-runtime";
 import { cn } from "@/common/lib/utils";
 import { usePlayer } from "@/player/use-player";
-import { Button } from "@/widget/components/ui/button";
+import { Button, buttonVariants } from "@/widget/components/ui/button";
 import { Tooltip } from "@/widget/components/ui/tooltip";
 import {
 	EmotionsIcon,
-	GovBRIcon,
 	PauseIcon,
 	PlayIcon,
 	ReplayIcon,
 	SettingsIcon,
 	SubtitleOffIcon,
+	WaitingIcon,
 } from "@/widget/icons";
 import { SubtitleIcon } from "@/widget/icons/subtitle";
 import { useScreensStore } from "@/widget/stores/use-screens.store";
@@ -18,79 +19,75 @@ import { useWidgetStore } from "@/widget/stores/use-widget.store";
 
 export const WidgetControls = () => {
 	const { open } = useScreensStore();
-	const { play, pause, status, repeat, gloss, countGloss, showSubtitles, toggleSubtitles } = usePlayer();
 	const { isOpen } = useWidgetStore();
+	const { play, pause, status, repeat, isPlayingWelcome, gloss, countGloss, showSubtitles, toggleSubtitles } =
+		usePlayer();
 
-	const progress = (countGloss.count / Math.max(countGloss.max, countGloss.count)) * 100;
+	const progress = useMemo(() => {
+		if (countGloss.max === 0) return 0;
+		return (countGloss.count / Math.max(countGloss.max, countGloss.count)) * 100;
+	}, [countGloss]);
 
 	return (
 		<Fragment>
 			<div
 				className={cn(
-					"absolute inset-2 xs:inset-4 top-auto! mx-auto flex animate-move-up items-center gap-1 transition-[bottom] ease-in-out",
+					"absolute inset-4 top-auto! mx-auto flex animate-move-up items-center gap-1 transition-[bottom] ease-in-out",
 					"[&>div]:rounded-full [&>div]:p-1 [&_button]:animate-move-down [&_button]:rounded-full",
 					!isOpen && "-bottom-20!",
 				)}
 			>
 				<div
 					style={{ boxShadow: "0 0 10px rgba(0, 0, 0, 0.15)" }}
-					className={cn("flex w-full items-center gap-1 border bg-background")}
+					className={cn("mx-auto flex items-center gap-1 border bg-background")}
 				>
+					{status !== "playing" && !gloss && (
+						<div className={cn(buttonVariants({ size: "icon", variant: "ghost-gov" }), "w-16 rounded-full")}>
+							<WaitingIcon className="size-5 opacity-50" />
+						</div>
+					)}
+
 					{status === "paused" && (
 						<Tooltip
-							className="text-xs **:data-[slot=arrow-container]:left-1"
+							className="text-xs"
 							offset={2}
 							variant="primary"
 							content="Reproduzir"
+							disabled={!gloss}
 							placement="top"
-							align="start"
-							arrow={{ position: "bottom-left" }}
+							arrow={{ position: "bottom" }}
 						>
-							<Button onClick={() => play()} variant="default" size="icon">
+							<Button disabled={!gloss} className="w-16" onClick={() => play()} variant="default" size="icon">
 								<PlayIcon />
 							</Button>
 						</Tooltip>
 					)}
 
 					{status === "idle" && Boolean(gloss) && (
-						<Tooltip
-							className="text-xs **:data-[slot=arrow-container]:left-1"
-							offset={2}
-							content="Repetir"
-							placement="top"
-							align="start"
-							arrow={{ position: "bottom-left" }}
-						>
-							<Button onClick={() => repeat()} variant="ghost-gov" size="icon" className="group">
+						<Tooltip className="text-xs" offset={2} content="Repetir" placement="top" arrow={{ position: "bottom" }}>
+							<Button onClick={() => repeat()} variant="ghost-gov" size="icon" className="group w-16">
 								<ReplayIcon />
 							</Button>
 						</Tooltip>
 					)}
 
 					{status === "playing" && (
-						<Tooltip
-							className="text-xs **:data-[slot=arrow-container]:left-1"
-							offset={2}
-							content="Pausar"
-							placement="top"
-							align="start"
-							arrow={{ position: "bottom-left" }}
-						>
-							<Button onClick={pause} variant="ghost-gov" size="icon">
+						<Tooltip className="text-xs" offset={2} content="Pausar" placement="top" arrow={{ position: "bottom" }}>
+							<Button className="w-16" onClick={pause} variant="ghost-gov" size="icon">
 								<PauseIcon />
 							</Button>
 						</Tooltip>
 					)}
 
-					<span className="mx-auto px-1">
-						<GovBRIcon className="h-4 w-fit" />
-					</span>
+					<div className="mx-2 -my-4 h-6 w-0.5 rounded-full bg-border" />
 
-					<Tooltip className="text-xs" offset={2} content="Emoções" placement="top" arrow={{ position: "bottom" }}>
-						<Button variant="ghost-gov" size="icon">
-							<EmotionsIcon />
-						</Button>
-					</Tooltip>
+					<div className="ml-auto">
+						<Tooltip className="text-xs" offset={2} content="Emoções" placement="top" arrow={{ position: "bottom" }}>
+							<Button variant="ghost-gov" size="icon">
+								<EmotionsIcon />
+							</Button>
+						</Tooltip>
+					</div>
 
 					<Tooltip
 						className="whitespace-nowrap text-xs"
@@ -99,13 +96,14 @@ export const WidgetControls = () => {
 						content={showSubtitles ? "Desativar legendas" : "Ativar legendas"}
 						placement="top"
 						arrow={{ position: "bottom-right" }}
+						disabled={isPlayingWelcome}
 					>
-						<Button onClick={() => toggleSubtitles()} variant="ghost-gov" size="icon">
+						<Button disabled={isPlayingWelcome} onClick={() => toggleSubtitles()} variant="ghost-gov" size="icon">
 							{showSubtitles ? <SubtitleIcon /> : <SubtitleOffIcon />}
 						</Button>
 					</Tooltip>
 
-					<Button onClick={() => open("settings")} variant="ghost-gov" size="icon">
+					<Button disabled={isPlayingWelcome} onClick={() => open("settings")} variant="ghost-gov" size="icon">
 						<SettingsIcon />
 					</Button>
 				</div>
