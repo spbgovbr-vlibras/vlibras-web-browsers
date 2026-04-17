701ae6daf4714b6c3128e0c3fe12074d91361885
Diêgo Ferreira <diegofrr.19@gmail.com>
2026-04-17 02:43:39 -0300

    refactor(widget): remove unused dialog component

--- src/widget/providers/app.tsx
+++ src/widget/providers/app.tsx
@@ -1,12 +1,10 @@
 import { Fragment } from "preact/jsx-runtime";
-import { DialogProvider } from "./dialog";
 import { ScreensProvider } from "./screens";
 
 export const WidgetProviders = () => {
 	return (
 		<Fragment>
 			<ScreensProvider />
-			<DialogProvider />
 		</Fragment>
 	);
 };
