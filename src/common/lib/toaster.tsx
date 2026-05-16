ce4d2e22eebe525dfc4f802b23f4f5f72230af71
Diêgo Ferreira <diegofrr.19@gmail.com>
2026-05-16 04:08:36 -0300

    chore: merge feat/v7 into feat/widget-v7

--- src/common/lib/toaster.tsx
+++ src/common/lib/toaster.tsx
@@ -1,8 +1,9 @@
+import type { ComponentChildren } from "preact";
 import { randomStr } from "@/common/utils";
 
 export type ToastData = ToastOptions & {
 	id: string;
-	message: string;
+	message: string | ComponentChildren;
 	isExiting?: boolean;
 };
 
@@ -13,6 +14,7 @@ type ToastOptions = {
 	position?: "top" | "bottom";
 	align?: "center" | "start" | "end";
 	variant?: "success" | "destructive" | "default";
+	className?: string;
 };
 
 let toasts: ToastData[] = [];
@@ -20,7 +22,7 @@ let listeners: Listener[] = [];
 
 const notify = () => listeners.forEach((l) => l([...toasts]));
 
-export const toast = (message: string, options?: ToastOptions) => {
+export const toast = (message: string | ComponentChildren, options?: ToastOptions) => {
 	const id = randomStr();
 	const duration = options?.duration || 3000;
 
