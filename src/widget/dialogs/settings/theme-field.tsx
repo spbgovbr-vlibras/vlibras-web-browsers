6bdcba6053d0f293f4b86c6a15dd7aab02595a7c
Diêgo Ferreira <diegofrr.19@gmail.com>
2026-07-17 11:20:51 -0300

    feat(settings): add posthog tracking for theme toggle

--- src/widget/dialogs/settings/theme-field.tsx
+++ src/widget/dialogs/settings/theme-field.tsx
@@ -1,10 +1,19 @@
 import { useTheme } from "@/common/hooks";
+import { posthogg } from "@/common/lib/posthog";
 import { InlineTranslatorButton } from "@/widget/components/inline-translator-button";
 import { useSettingsCtx } from "./context";
 
 export const SettingsThemeField = () => {
-	const { theme, toggleTheme } = useTheme();
-	const { onOpen } = useSettingsCtx();
+	const theme = useTheme((s) => s.theme);
+	const onOpen = useSettingsCtx((s) => s.onOpen);
+
+	const handleToggleTheme = () => {
+		const { theme, toggleTheme } = useTheme.getState();
+		const newTheme = theme === "dark" ? "Claro" : "Escuro";
+
+		toggleTheme();
+		posthogg.trackEvent("theme", { theme: newTheme });
+	};
 
 	return (
 		<div className="flex w-full items-start justify-between">
@@ -15,7 +24,7 @@ export const SettingsThemeField = () => {
 
 			<input
 				type="checkbox"
-				onChange={toggleTheme}
+				onChange={handleToggleTheme}
 				checked={theme === "dark"}
 				className="toggle toggle-sm rounded-full bg-accent text-primary before:size-4 before:rounded-full not-checked:before:bg-background checked:bg-primary checked:text-primary-foreground"
 			/>
