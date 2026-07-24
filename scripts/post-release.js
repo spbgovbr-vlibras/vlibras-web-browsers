3ebf54461722328ec1f5ef001f173c2fdd0691e2
Diêgo Ferreira <diegofrr.19@gmail.com>
2026-07-24 10:28:09 -0300

    chore(release): update version badge regex in post-release script

--- scripts/post-release.js
+++ scripts/post-release.js
@@ -12,7 +12,8 @@ const newVersion = packageJson.version;
 
 let readmeContent = fs.readFileSync(readmeFilePath, "utf8");
 
-const versionRegexReadme = /!\[Version\]\(https:\/\/img.shields.io\/badge\/version-[^ ]+-blue\)/;
+const versionRegexReadme =
+  /!\[Version\]\(https:\/\/img\.shields\.io\/badge\/version-[^)]+-blue\)/;
 const newVersionBadge = `![Version](https://img.shields.io/badge/version-${newVersion.replace(/-/g, "_")}-blue)`;
 readmeContent = readmeContent.replace(versionRegexReadme, newVersionBadge);
 
