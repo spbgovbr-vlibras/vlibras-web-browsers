1fb0cc828e734fe6da9fe2647964f0d74be7a912
Diêgo Ferreira <diegofrr.19@gmail.com>
2026-08-03 17:35:03 -0300

    feat(widget): display "upset" emotion in emotions option

--- src/widget/components/controls/emotions-option.tsx
+++ src/widget/components/controls/emotions-option.tsx
@@ -50,9 +50,6 @@ export const EmotionsOption = () => {
 							const emotion = emotionsMap[key];
 							const isActive = emotion === currentEmotion;
 
-							// Não exibe a emoção "Chateado"
-							if (key === "upset") return null;
-
 							return (
 								<li key={key}>
 									<button
