648ed2e90b809e2ef04d31c4c1f9e8d0a5a07d4b
Diêgo Ferreira <diegofrr.19@gmail.com>
2026-08-31 16:48:13 -0300

    chore(release/extensions): bump version to 7.11.0 and automate manifest updates

--- scripts/post-release.js
+++ scripts/post-release.js
@@ -4,11 +4,26 @@ import { execSync } from "node:child_process";
 
 const cwd = process.cwd();
 
+const CHROME_MANIFEST = "extensions/chrome/manifest.json";
+const FIREFOX_MANIFEST = "extensions/firefox/manifest.json";
+
 const packageJsonPath = path.join(cwd, "package.json");
 const readmeFilePath = path.join(cwd, "README.md");
+const chromeManifestPath = path.join(cwd, CHROME_MANIFEST);
+const firefoxManifestPath = path.join(cwd, FIREFOX_MANIFEST);
 
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
 const newVersion = packageJson.version;
+
+for (const manifestPath of [chromeManifestPath, firefoxManifestPath]) {
+  const manifest = JSON.parse(fs.readFileSync(manifestPath, "utf8"));
+  manifest.version = newVersion;
+  fs.writeFileSync(
+    manifestPath,
+    `${JSON.stringify(manifest, null, "\t")}\n`,
+    "utf8",
+  );
+}
 
 let readmeContent = fs.readFileSync(readmeFilePath, "utf8");
 
@@ -22,5 +37,8 @@ const yearRegexReadme = /(Suíte%20VLibras-)[^-]+(-green\.svg)/;
 readmeContent = readmeContent.replace(yearRegexReadme, `$1${currentYear}$2`);
 
 fs.writeFileSync(readmeFilePath, readmeContent, "utf8");
-execSync("git add package.json README.md CHANGELOG.md", { stdio: "inherit" });
+execSync(
+  `git add package.json README.md CHANGELOG.md ${CHROME_MANIFEST} ${FIREFOX_MANIFEST}`,
+  { stdio: "inherit" },
+);
 execSync(`git commit -m "chore: release v${newVersion}"`, { stdio: "inherit" });
